686112bf25416a01720a18f029404098dafb96cc
Andrei Chernykh <andrei.chernykh@gmail.com>
2016-01-29 02:24:16 +0300

    #convert_to method implemented

--- lib/currency_converter/money.rb
+++ lib/currency_converter/money.rb
@@ -16,22 +16,57 @@ module CurrencyConverter
 
     def self.conversion_rates(base_currency, rates={})
       @configuration = Configuration.new(base_currency, rates)
+      self
     end
 
     attr_reader :amount, :currency
 
     def initialize(amount, currency)
-      @amount = amount
+      raise_unknown_currency unless known_currency?(currency)
+      @amount = amount.to_f.round(2)
       @currency = currency
     end
 
     def inspect
-      "#{amount} #{currency}"
+      "#{'%.2f' % amount} #{currency}"
     end
 
     def to_s
       inspect
     end
 
+    def convert_to(currency_name)
+      raise_unknown_currency unless known_currency?(currency_name)
+
+      return self if currency_name == currency
+
+      converted_amount = convert_amount_to(currency_name)
+      self.class.new(converted_amount, currency_name)
+    end
+
+    private
+
+    def known_currency?(currency)
+      config = self.class.configuration
+      config.base_currency == currency || config.rates.keys.include?(currency)
+    end
+
+    def raise_unknown_currency
+      raise ArgumentError.new('Unknown currency. Please configure via .conversion_rates')
+    end
+
+    def convert_amount_to(to_currency)
+      config = self.class.configuration
+
+      if self.currency == config.base_currency
+        self.amount * config.rates[to_currency]
+      else
+        if to_currency == config.base_currency
+          self.amount / config.rates[self.currency]
+        else
+          self.amount / config.rates[self.currency] * config.rates[to_currency]
+        end
+      end
+    end
   end
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -2,20 +2,24 @@ require 'spec_helper'
 
 describe CurrencyConverter::Money do
 
-  let(:money) { described_class.new(123.4, 'QWE') }
+  BASE_CURRENCY = 'EUR'
+  RATES = {'USD' => 1.11, 'Bitcoin' => 0.0047}
+  UNKNOWN_CURRENCY = 'QWE'
+  UNKNOWN_CURRENCY_ERROR_MESSAGE = 'Unknown currency. Please configure via .conversion_rates'
+
+  before do
+    described_class.conversion_rates(BASE_CURRENCY, RATES)
+  end
 
   describe '.conversion_rates' do
 
-    context 'if base currency was not provided' do
-      it 'raises an error' do
-        expect { described_class.conversion_rates }.to raise_error(ArgumentError)
-      end
+    it 'allows to define conversion rates' do
+      expect(described_class.configuration.base_currency).to eq(BASE_CURRENCY)
+      expect(described_class.configuration.rates).to eq(RATES)
     end
 
-    it 'allows to define conversion rates' do
-      described_class.conversion_rates('EUR', {'USD' => 1.11, 'Bitcoin' => 0.0047})
-      expect(described_class.configuration.base_currency).to eq('EUR')
-      expect(described_class.configuration.rates).to eq({'USD' => 1.11, 'Bitcoin' => 0.0047})
+    it 'returns configured class' do
+      expect(described_class.conversion_rates(BASE_CURRENCY)).to eq(CurrencyConverter::Money)
     end
 
     it 'stringify base currency' do
@@ -24,33 +28,105 @@ describe CurrencyConverter::Money do
     end
 
     it 'stringify currency names in rates' do
-      described_class.conversion_rates('EUR', {usd: 1.11})
+      described_class.conversion_rates(BASE_CURRENCY, {usd: 1.11})
       expect(described_class.configuration.rates['usd']).to eq(1.11)
     end
   end
 
   describe '.new' do
 
+    it 'raises an error with unknown provided currency' do
+      expect { described_class.new(123.4, UNKNOWN_CURRENCY) }.to raise_error(ArgumentError, UNKNOWN_CURRENCY_ERROR_MESSAGE)
+    end
+
     it 'creates an instance with provided amount' do
-      expect(money.amount).to eq(123.4)
+      expect(described_class.new(123.4, BASE_CURRENCY).amount).to eq(123.4)
+    end
+
+    it 'cast amount to float' do
+      expect(described_class.new(123, BASE_CURRENCY).amount).to be_a(Float)
+    end
+
+    it 'stores amount with precision = 2' do
+      expect(described_class.new(123.456, BASE_CURRENCY).amount).to eq(123.46)
     end
 
     it 'creates an instance with provided currency' do
-      expect(money.currency).to eq('QWE')
+      expect(described_class.new(123.4, BASE_CURRENCY).currency).to eq(BASE_CURRENCY)
     end
+
   end
+
+  let(:money) { described_class.new(50, BASE_CURRENCY) }
 
   describe '#inspect' do
 
     it 'returns human representation' do
-      expect(money.inspect).to eq("#{money.amount} #{money.currency}")
+      expect(money.inspect).to eq("#{'%.2f' % money.amount} #{money.currency}")
     end
   end
 
   describe '#to_s' do
 
     it 'returns human representation' do
-      expect(money.to_s).to eq("#{money.amount} #{money.currency}")
+      expect(money.to_s).to eq("#{'%.2f' % money.amount} #{money.currency}")
+    end
+  end
+
+  describe '#convert_to' do
+
+    it 'raises an error if unknown currency was provided' do
+      expect { money.convert_to(UNKNOWN_CURRENCY) }.to raise_error(ArgumentError, UNKNOWN_CURRENCY_ERROR_MESSAGE)
+    end
+
+    it 'returns Money instance' do
+      expect(money.convert_to('USD')).to be_a(described_class)
+    end
+
+    context 'while converting to a self currency' do
+
+      it 'returns itself' do
+        expect(money.convert_to(money.currency).__id__).to eq(money.__id__)
+      end
+    end
+
+    context 'while converting to a different currency' do
+
+      let(:converted_money) { money.convert_to('USD') }
+
+      it 'returns a new instance' do
+        expect(converted_money.__id__).not_to eq(money.__id__)
+      end
+
+      it 'returns instance with a different currency' do
+        expect(converted_money.currency).to eq('USD')
+      end
+
+      context 'if current currency equals to base conversion currency' do
+
+        it "returns amount multiplied by 'converted to' currency's rate" do
+          expect(converted_money.amount).to eq(55.50)
+        end
+      end
+
+      context 'if current currency differs from base conversion currency' do
+
+        let(:money) { described_class.new(55.50, 'USD') }
+
+        context 'if converted to base conversion currency' do
+
+          it "returns amount devided by current currency's rate" do
+            expect(money.convert_to(BASE_CURRENCY).amount).to eq(50.00)
+          end
+        end
+
+        context 'if converted not to base conversion currency' do
+
+          it "returns amount devided by current currency's rate and multiplied by 'converted to' currency's rate" do
+            expect(money.convert_to('Bitcoin').amount).to eq(0.24)
+          end
+        end
+      end
     end
   end
 end
